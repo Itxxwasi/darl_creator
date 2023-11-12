82321817e4660f56c053d0a205d8b0ce4f40b6db
Itxxwasi <142110420+Itxxwasi@users.noreply.github.com>
2023-11-12 18:00:20 +0500

    WASI TECH

--- config.js
+++ config.js
@@ -7,18 +7,18 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/yasiyaofc1/FORZEN-MD'
+global.github = 'https://github.com/Itxxwasi/darl_creator'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '94760018802'
-global.devs = '94760018802';
-global.website = 'https://github.com/yasiyaofc1/FORZEN-MD' //wa.me/+94760018802
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/0b5a7839e8ee21874921e.jpg'
+global.sudo = process.env.SUDO || '923192173398'
+global.devs = '923259823531';
+global.website = 'https://github.com/Itxxwasi/darl_creator/edit' //wa.me/+94760018802
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/10ef9a9c890be1ae07c22.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'FORZEN MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'YASIYA OFC' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? 'WASI MD' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'WASI TECH' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'YASIYA OFC' : process.env.PACK_INFO.split(";")[0],
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'ITX_ME_WASI' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'FORZEN MD' : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
