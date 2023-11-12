8f67462d6d33d91fb28f1ef48fd26d11c2f16ed2
Itxxwasi <142110420+Itxxwasi@users.noreply.github.com>
2023-11-12 17:14:39 +0500

    WASI TECH

--- plugin/Auto_voice.js
+++ plugin/Auto_voice.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : FORZEN-MD
- * @author : YASIYA-OFC <https://github.com/yasiyaofc1>
- * @description : FORZEN-MD,A Multi-functional whatsapp bot.
+ * @project_name : WASI
+ * @author : WASI<https://github.com/itxxwasi>
+ * @description : WASI TECH,A Multi-functional whatsapp bot.
  * @version 1.0.0
  **/
 
